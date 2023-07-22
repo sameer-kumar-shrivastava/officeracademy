c75031d22ab82438fe60c73defd77333b188f5e5
Sameer Kumar Shrivastava <mail.sameershrivastava@gmail.com>
2023-07-22 12:05:08 +0530

    Added About page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar.component';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Home from './Pages/Home.pages';
+import Home from './Pages/Home/Home.pages';
+import About from './Pages/AboutUs/About.pages';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/"  component={<Home/>} />
+        <Route path="/about" component={<About/>} />
         {/* <Route path="/courses" component={Navbar} /> */}
         {/* Add more routes for other pages */}
       </Routes>
---
+++ src/Pages/AboutUs/About.pages.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () =>{
+    return(<><h1>About USSSS</h1></>);
+    }
+
+    export default About;
