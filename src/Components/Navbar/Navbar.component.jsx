2b65b5044d8a887e4cf1db083f3c6bbf8d72be49
Sameer Kumar Shrivastava <mail.sameershrivastava@gmail.com>
2023-07-22 11:57:17 +0530

    Added navbar

---
+++ src/Components/Navbar/Navbar.component.jsx
@@ -0,0 +1,47 @@
+// Navbar.js
+import React from 'react';
+import './Navbar.styles.scss';
+
+import { Link } from 'react-router-dom'; // If you are using React Router for navigation
+
+
+const Navbar = () => {
+  const isLoggedIn = false; // Replace this with your authentication state
+
+  const handleLogout = () => {
+    // Implement your logout logic here
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <Link to="/" className="navbar-logo">
+          Officer Academy
+        </Link>
+        <ul className="navbar-menu">
+        
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/privacy">Privacy Policy</Link>
+          </li>
+          <li>
+            <Link to="/mynotes">My Notes</Link>
+          </li>
+          {isLoggedIn ? (
+            <li>
+              <button onClick={handleLogout}>Logout</button>
+            </li>
+          ) : (
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          )}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/Pages/Home.pages.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Home.styles.scss';
+
+const Home = () =>{
+        return (
+        <>
+            
+           <h1>Heyyyyyy Helllo</h1> 
+    
+        </>
+    );
+}
+
+export default Home;
